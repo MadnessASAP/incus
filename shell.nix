c852b597e79d94563bc315b5c42c228967190d54
Michael 'ASAP' Weinrich <michael@a5ap.net>
2024-12-17 11:25:07 -0800

    fixup! add shell.nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,31 @@
 let
-  pkgs = import <nixpkgs> { };
+  pkgs = import <nixpkgs> { inherit overlays; };
+  _xgettext-go =
+    { buildGoModule
+    , fetchFromGitHub
+    , gettext
+    }: buildGoModule rec {
+      pname = "xgettext-go";
+      version = "2.57.1";
+
+      src = fetchFromGitHub {
+        owner = "canonical";
+        repo = "snapd";
+        rev = version;
+        hash = "sha256-icPEvK8jHuJO38q1n4sabWvdgt9tB5b5Lh5/QYjRBBQ=";
+      };
+
+      vendorHash = "sha256-e1QFZIleBVyNB0iPecfrPOg829EYD7d3KMHIrOYnA74=";
+      subPackages = [
+        "i18n/xgettext-go"
+      ];
+    };
+  overlays = [
+    (final: prev: {
+      xgettext-go = final.callPackage _xgettext-go { };
+    })
+  ];
+
 in
 pkgs.mkShell {
   packages = with pkgs; [
@@ -10,7 +36,11 @@ pkgs.mkShell {
 
     # static-analysis
     debianutils
+    go-licenses
+    go-swagger
+    gettext
     shellcheck
+    xgettext-go
     (python3.withPackages (pyPkgs: with pyPkgs; [
       flake8
     ]))
